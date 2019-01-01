be3604f962b65ba95c44a2d0aa38241fef7b2cf7
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-01-01 17:39:36 +0100

    Fix the mappings a bit

--- src/pin_mappings.rs
+++ src/pin_mappings.rs
@@ -5,7 +5,7 @@ use crate::gpio::gpiob::*;
 #[allow(unused)]
 #[cfg(feature = "device-selected")]
 use crate::gpio::gpioc::*;
-#[cfg(feature = "stm32f030xc")]
+#[cfg(feature = "stm32f030")]
 use crate::gpio::gpiod::*;
 #[allow(unused)]
 #[cfg(feature = "device-selected")]
@@ -58,6 +58,41 @@ pins! {
     PA12 => {AF5: SdaPin<I2C1>}
 }
 
+#[cfg(any(feature = "stm32f030", feature = "stm32f070"))]
+pins! {
+    PA0 => {AF4: TxPin<USART4>},
+    PA1 => {AF4: RxPin<USART4>},
+    PB10 => {
+        AF4: TxPin<USART3>,
+        AF5: SckPin<SPI2>
+    },
+    PB11 => {AF4: RxPin<USART3>},
+    PC2 => {AF1: MisoPin<SPI2>},
+    PC3 => {AF1: MosiPin<SPI2>},
+    PC4 => {AF1: TxPin<USART3>},
+    PC5 => {AF1: RxPin<USART3>},
+    PC10 => {
+        AF0: TxPin<USART4>,
+        AF1: TxPin<USART3>
+    },
+    PC11 => {
+        AF0: RxPin<USART4>,
+        AF1: RxPin<USART3>
+    }
+}
+
+#[cfg(feature = "stm32f030")]
+pins! {
+    PA4 => {AF5: TxPin<USART6>},
+    PA5 => {AF5: RxPin<USART6>},
+    PB3 => {AF4: TxPin<USART5>},
+    PB4 => {AF4: RxPin<USART5>},
+    PC0 => {AF2: TxPin<USART6>},
+    PC1 => {AF2: RxPin<USART6>},
+    PC12 => {AF2: RxPin<USART5>},
+    PD2 => {AF2: TxPin<USART5>}
+}
+
 #[cfg(feature = "stm32f030x6")]
 pins! {
     PA2 => {AF1: TxPin<USART1>},
@@ -81,42 +116,6 @@ pins! {
     PA14 => {AF1: TxPin<USART2>},
     PA15 => {AF1: RxPin<USART2>}
 }
-
-#[cfg(any(feature = "stm32f030xc", feature = "stm32f070xb"))]
-pins! {
-    PA0 => {AF4: TxPin<USART4>},
-    PA1 => {AF4: RxPin<USART4>},
-    PB10 => {
-        AF4: TxPin<USART3>,
-        AF5: SckPin<SPI2>
-    },
-    PB11 => {AF4: RxPin<USART3>},
-    PC2 => {AF1: MisoPin<SPI2>},
-    PC3 => {AF1: MosiPin<SPI2>},
-    PC4 => {AF1: TxPin<USART3>},
-    PC5 => {AF1: RxPin<USART3>},
-    PC10 => {
-        AF0: TxPin<USART4>,
-        AF1: TxPin<USART3>
-    },
-    PC11 => {
-        AF0: RxPin<USART4>,
-        AF1: RxPin<USART3>
-    }
-}
-
-#[cfg(feature = "stm32f030xc")]
-pins! {
-    PA4 => {AF5: TxPin<USART6>},
-    PA5 => {AF5: RxPin<USART6>},
-    PB3 => {AF4: TxPin<USART5>},
-    PB4 => {AF4: RxPin<USART5>},
-    PC0 => {AF2: TxPin<USART6>},
-    PC1 => {AF2: RxPin<USART6>},
-    PC12 => {AF2: RxPin<USART5>},
-    PD2 => {AF2: TxPin<USART5>}
-}
-
 #[cfg(any(
     feature = "stm32f030x8",
     feature = "stm32f030xc",
@@ -132,7 +131,7 @@ pins! {
     feature = "stm32f030x6",
     feature = "stm32f030xc",
     feature = "stm32f042",
-    feature = "stm32f070x6",
+    feature = "stm32f070x6"
 ))]
 pins! {
     PA9 => {AF4: SclPin<I2C1>},
@@ -140,10 +139,8 @@ pins! {
 }
 
 #[cfg(any(
-    feature = "stm32f042",
     feature = "stm32f030x6",
-    feature = "stm32f030x8",
-    feature = "stm32f030xc",
+    feature = "stm32f042",
     feature = "stm32f070xb"
 ))]
 pins! {
@@ -151,19 +148,23 @@ pins! {
     PB11 => {AF1: SdaPin<I2C1>}
 }
 
+#[cfg(any(feature = "stm32f030x8", feature = "stm32f030xc"))]
+pins! {
+    PB10 => {AF1: SclPin<I2C2>},
+    PB11 => {AF1: SdaPin<I2C1>}
+}
 #[cfg(any(
     feature = "stm32f042",
     feature = "stm32f030xc",
     feature = "stm32f070x6",
 ))]
 pins! {
-    PF1 => {AF1: SclPin<I2C1>},
     PF0 => {AF1: SdaPin<I2C1>}
+    PF1 => {AF1: SclPin<I2C1>},
 }
 
 #[cfg(any(
     feature = "stm32f042",
-    feature = "stm32f030xc",
     feature = "stm32f030xc",
     feature = "stm32f070xb"
 ))]

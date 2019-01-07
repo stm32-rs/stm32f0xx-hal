ad7c7dfe8fcd4f1b034e1c9e467a24cf16f3ee53
HarkonenBade <github@harkonen.net>
2019-01-07 17:04:42 +0000

    Test using a feature detecting check script

---
+++ tools/check.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+import json
+import subprocess
+import sys
+
+
+def run_inner(args):
+    print("Running `{}`...".format(" ".join(args)))
+    ret = subprocess.run(args).returncode == 0
+    print("")
+    return ret
+
+
+def run(mcu):
+    if mcu == "":
+        return run_inner(["cargo", "check"])
+    else:
+        return run_inner(["cargo",
+                          "check",
+                          "--examples",
+                          "--features={}".format(mcu)])
+
+
+def main():
+    cargo_meta = json.loads(
+        subprocess.run("cargo metadata --no-deps --format-version=1",
+                       shell=True,
+                       stdout=subprocess.PIPE,
+                       universal_newlines=True).stdout
+        )
+
+    crate_info = cargo_meta["packages"][0]
+
+    features = [""] + ["{} rt".format(x)
+                       for x in crate_info["features"].keys()
+                       if x != "device-selected" and x != "rt"]
+
+    if not all(map(run, features)):
+        sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
+

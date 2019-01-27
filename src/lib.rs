1e040f403b155ebc3fd8ac30f1ac60f55f4a4abd
Daniel Egger <daniel@eggers-club.de>
2019-01-27 21:05:35 +0100

    Implement TSC (touch sensitive controller) support
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/lib.rs
+++ src/lib.rs
@@ -46,5 +46,19 @@ pub mod spi;
 pub mod time;
 #[cfg(feature = "device-selected")]
 pub mod timers;
+#[cfg(any(
+    feature = "stm32f031",
+    feature = "stm32f051",
+    feature = "stm32f071",
+    feature = "stm32f091",
+    feature = "stm32f042",
+    feature = "stm32f072",
+    feature = "stm32f038",
+    feature = "stm32f048",
+    feature = "stm32f058",
+    feature = "stm32f078",
+    feature = "stm32f098",
+))]
+pub mod tsc;
 #[cfg(feature = "device-selected")]
 pub mod watchdog;

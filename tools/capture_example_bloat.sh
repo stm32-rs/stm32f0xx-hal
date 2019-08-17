a34f6c84e66f8244313a492ae28f53d73dffff62
Daniel Egger <daniel@eggers-club.de>
2019-08-17 16:59:44 +0200

    Let capture_example_bloat script also capture dev mode sizes
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- tools/capture_example_bloat.sh
+++ tools/capture_example_bloat.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 filename="bloat_log_"`date -Iminutes`".txt"
+filenamenoopt="bloat_noopt_log_"`date -Iminutes`".txt"
 
 for i in `find examples -name "*.rs"`; do
         name=$(echo $i | sed -e "s,examples/,,g" -e "s,\.rs,,g")
         echo "Processing example $name"
         echo >>$filename
+        echo >>$filenamenoopt
         echo "Bloat for example $name" >>$filename
-        cargo bloat --release --features=stm32f042,rt --example $name >>$filename
+        echo "Bloat for example $name" >>$filenamenoopt
+        cargo bloat --release --example $name --features="stm32f042,rt" -n 60 >>$filename
+        cargo bloat --example $name --features="stm32f042,rt" -n 60 >>$filenamenoopt
 done
 
 echo "Captures bloat for all examples into $filename"
